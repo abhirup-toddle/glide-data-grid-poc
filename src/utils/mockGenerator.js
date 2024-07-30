fe8d0c9bf9099f53548abb840353aad4bda9025b
Abhirup Bardhan <abhirup.bardhan@toddleapp.com>
2024-07-30 13:20:05 +0530

    chore: Add new projects to mockGenerator.js

--- src/utils/mockGenerator.js
+++ src/utils/mockGenerator.js
@@ -82,6 +82,36 @@ const emailProviders = [
   "bellsouth.net",
 ];
 
+const projects = [
+  "Solar System",
+  "Human Body",
+  "Geology",
+  "Chemistry",
+  "Physics",
+  "Astronomy",
+  "Mathematics",
+  "History",
+  "Literature",
+  "Art",
+  "Music",
+  "Dance",
+  "Theater",
+  "Film",
+  "Photography",
+  "Journalism",
+  "Robotics",
+  "Engineering",
+  "Architecture",
+];
+
+export function generateRandomNumberBetween(min = 10, max = 100) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export function getRandomProject() {
+  return getRandomElement(projects);
+}
+
 function getRandomElement(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
