0b371f29d98b2a10804b00a3f8a40f42531a069a
Abhirup Bardhan <abhirup.bardhan@toddleapp.com>
2024-07-30 13:20:20 +0530

    chore: Update starterData.js with score data and columns

--- src/utils/starterData.js
+++ src/utils/starterData.js
@@ -46,3 +46,50 @@ export const _columns = [
     id: "phone",
   },
 ];
+
+export const _scoreData = [
+  {
+    name: "Hines Fowler",
+    project: "Photosynthesis",
+    score: 100,
+    attendance: "70%",
+  },
+  {
+    name: "Mccray Mcleod",
+    company: "ZOLARITY",
+    email: "mcray",
+    phone: "+1 (972) 546-3983",
+  },
+  {
+    name: "Carmen Mcbride",
+    company: "ZUVY",
+    email: "",
+    phone: "+1 (944) 546-3983",
+  },
+];
+
+export const _scoreColumns = [
+  {
+    title: "Name",
+    id: "name",
+    themeOverride: {
+      bgIconHeader: "#00967d",
+      textDark: "#00c5a4",
+      textHeader: "#00c5a4",
+      // bgCell: "pink",
+    },
+  },
+  {
+    title: "Score",
+    id: "score",
+    icon: GridColumnIcon.HeaderImage,
+  },
+  {
+    title: "Email",
+    id: "email",
+  },
+  {
+    title: "Phone",
+    id: "phone",
+  },
+];
